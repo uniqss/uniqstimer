3e1d4cabadfd3b7aa6ba9e77341fd31ff6d3969c
uniqss <uniqs@163.com>
2022-01-21 00:47:01 +0800

    性能很糟糕，赶紧提升

--- cpp_test/main_logicthread.cpp
+++ cpp_test/main_logicthread.cpp
@@ -22,31 +22,46 @@ void LogicThread() {
 #endif
 
 #if 1
-    for (TimerIdType i = 1; i <= 100000; ++i) {
-        pMgrIII->CreateTimer(i, OnTimerIIIPressureTest, (void*)"timer LV III timer test pressure test", 1000, 1000);
+    for (TimerIdType i = 1; i <= 200000; ++i) {
+        pMgrIII->CreateTimer(i, OnTimerIIIPressureTest, (void*)"timer LV III timer test pressure test", (i % 1000) + 1, 1000);
     }
 #endif
 
     int64_t lastMS = UTimerGetCurrentTimeMS();
     int64_t currMS = 0;
+    
+    int64_t tmpDiffSum = 0;
+    int lessCount = 0;
+    int exceedCount = 0;
     while (bWorking) {
-        ++RunCount;
         FrameOnTimerCalled = 0;
         //pMgr->Run();
 
         currMS = UTimerGetCurrentTimeMS();
         while (currMS - lastMS >= 100) {
             int64_t tmpUSStart = UTimerGetCurrentTimeUS();
+
             pMgrIII->Run();
+            ++RunCount;
+            lastMS += 100;
+
             int64_t tmpUSEnd = UTimerGetCurrentTimeUS();
             int64_t tmpUSDiff = (tmpUSEnd - tmpUSStart) / 1000000;
             if (tmpUSDiff > 0) {
-                printf("exceed, cost:%llu \n", tmpUSDiff);
+                //printf("%llu\n", tmpUSDiff);
                 ++RunExceed1MSCount;
+                ++exceedCount;
+                tmpDiffSum += tmpUSDiff;
             } else {
-                printf(".");
+                //printf(".");
+                ++lessCount;
             }
-            lastMS += 100;
+            if (lessCount + exceedCount == 10) {
+                printf("%d|%lld ", exceedCount, tmpDiffSum);
+                lessCount = 0;
+                exceedCount = 0;
+                tmpDiffSum = 0;
+            }
         }
 
         std::this_thread::sleep_for(std::chrono::microseconds(10));
